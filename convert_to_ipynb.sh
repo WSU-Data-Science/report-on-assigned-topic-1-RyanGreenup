70d0ce68b6cd9fd032133ede29335cbe43476c62
ryangreenup <exogenesis@protonmail.com>
2021-04-17 13:40:45 +1000

    Improve TeX to ipynb conversion

--- convert_to_ipynb.sh
+++ convert_to_ipynb.sh
@@ -1,17 +1,25 @@
 
-## Convert the references to formatted text with haskell-citeproc
-pandoc groebner-bases-report.tex \
-    --bibliography=ref.bib --csl=~/Templates/CSL/nature.csl  --citeproc --standalone \
-    -o groebner-bases.ipynb
+FILENAME="groebner-bases.md"
 
 ## OR Just use markdown
 pandoc groebner-bases-report.tex        \
     --bibliography=ref.bib --standalone \
     -f latex -t markdown+raw_tex        \
-    -o groebner-bases.md
+    -o "${FILENAME}"
+
+    ## Fix the Headings 
+    sd '(^# .+) \{#.+\}$' '$1' "${FILENAME}"
+
+    ## Superscript the References
+    sd '(\[@.*\])' '<sup>  $1  </sup>' "${FILENAME}"
 
     ## Append the bibtex file
     echo '```bibtex' >> groebner-bases.md
     cat ref.bib      >> groebner-bases.md
     echo '```'       >> groebner-bases.md
 
+
+## Convert the references to formatted text with haskell-citeproc
+pandoc groebner-bases-report.tex \
+    --bibliography=ref.bib --csl=$HOME/Templates/CSL/nature.csl  --citeproc --standalone \
+    -o groebner-bases_citeproc.ipynb
